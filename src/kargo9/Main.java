943848cefb18d82b1a8d2bc84e072fd19e5299f8
rafetefe <rafet.gazanfer27@erzurum.edu.tr>
2020-05-18 18:39:55 +0300

    findSatir artık 2 boyutlu

--- src/kargo9/Main.java
+++ src/kargo9/Main.java
@@ -28,26 +28,38 @@ public class Main {
         return kullaniciList;
     }
     
-    public static String[] findSatir(
+    public static ArrayList<String[]> findSatir(
             ArrayList<String> Liste, int konum, String aranıcak){
         /*
         Konumlar={0 : ID,   1 : username,   2 : pass,   3 : yetki}
         */
+        
+        ArrayList<String[]> sonuclar = new ArrayList<String[]>();
+        
+        
         for (int i = 0; i < Liste.size(); i++) {
             String[] satir = Liste.get(i).split(" ");
             if (satir[konum].contains(aranıcak)){
                 //burda return, bulduğu ilk satırı döndürüyo,
                 //aranacak tutarlaştırılırsa kolaylaşır.
-                return satir;
+                sonuclar.add(satir);
             }
         }
+        return sonuclar;
         //
-        return null;
     }
   
     public static void main(String[] args) {
-
-        System.out.println(findSatir(getKullanicilar(), 2, "123"));
+        
+        //abomination
+        ArrayList<String[]> istek = findSatir(getKullanicilar(), 2, "123"); 
+        for (int i = 0; i < istek.size()  ; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print(istek.get(i)[j]);
+                
+            }
+            System.out.println("");
+        }
         //Açılacak ekranlar ve işleyiş
         //LoginFrame login = new LoginFrame();
         //login.setVisible(true);
